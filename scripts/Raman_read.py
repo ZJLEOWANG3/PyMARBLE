839b11dcc966ac2576a50a0f2fb66b2264300134
zjleowang <zw634@cornell.edu>
2023-02-06 12:48:18 -0500

    update Raman_read.py

--- scripts/Raman_read.py
+++ scripts/Raman_read.py
@@ -6,9 +6,12 @@
 from load import *
 
 def read_BG(path):
+        """
+        read separate background txt files
+        """
         df = pd.read_csv(path,header=None,sep='\t')
         X_BG = df.iloc[0,1:]
-        Y_BG = df.iloc[1:,1:].T#before transpose, shape of sample x feature
+        Y_BG = df.iloc[1:,1:]
         return X_BG,Y_BG
 
 def read_txt(path,type='point',BG=10):
@@ -18,18 +21,18 @@ def read_txt(path,type='point',BG=10):
         Default parameters are Raman type and number of background spectra
         'point' means point mapping, 'shape' means shape mapping.
         Return is X,Y for shape; X,Y,Y_BG for point
-        d x n
+        n x d
         """
         if type == 'shape':
             df = pd.read_csv(path,sep='\t',header=None)
-            X = df.iloc[0,2:].T
-            Y = df.iloc[1:,2:].T
+            X = df.iloc[0,2:]
+            Y = df.iloc[1:,2:]
             return X,Y
         if type == 'point': 
             df = pd.read_csv(path,sep='\t',header=None)
-            X = df.iloc[0,1:].T
-            Y = df.iloc[1:(df.shape[0]-BG),1:].T
-            Y_BG = df.iloc[(df.shape[0]-BG):,1:].T
+            X = df.iloc[0,1:]
+            Y = df.iloc[1:(df.shape[0]-BG),1:]
+            Y_BG = df.iloc[(df.shape[0]-BG):,1:]
             return X,Y,Y_BG
         if type == "dir":
             # read all the scRaman data in the directory
